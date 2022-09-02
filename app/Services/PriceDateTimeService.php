e8c1cc58a72e44c1d0f21156234b8c45931a13af
Jaedson <jaedson17@live.com>
2022-09-02 20:50:45 -0300

    Changes to how the api gets the price

--- app/Services/PriceDateTimeService.php
+++ app/Services/PriceDateTimeService.php
@@ -23,14 +23,16 @@ class PriceDateTimeService
 
     public function getPrice()
     {
-        $price = CryptoPrice::select('price', 'created_at')
+        $price = CryptoPrice::limit(1)
+                            ->select('price', 'created_at')
                             ->selectRaw('TO_CHAR(created_at, \'YYYY/MM/DD HH24:MI:SS\') as price_date')
                             ->where('crypto_id', $this->cryptoId)
-                            ->where('created_at', $this->dateTime)
+                            ->where('created_at', '>=', $this->dateTime)
+                            ->orderBy('id', 'asc')
                             ->get()
                             ->first();
 
-        if ($price === null) { // if there is no price at datetime, get the last price before that datetime
+        if ($price === null) { // if there is no price at datetime, get the last price after that datetime
             $price = CryptoPrice::limit(1)
                         ->select('price', 'created_at')
                         ->selectRaw('TO_CHAR(created_at, \'YYYY/MM/DD HH24:MI:SS\') as price_date')
